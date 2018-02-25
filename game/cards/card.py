ef14b5ea8b468cd9af3f38d262620d3ec3678904
Piotr Bielak <piotrbielak2@gmail.com>
2018-02-25 22:43:36 +0100

    Modified card "apply" method signature

--- game/cards/card.py
+++ game/cards/card.py
@@ -7,7 +7,7 @@ class Card(object):
     def __init__(self):
         pass
 
-    def apply(self, game_state):
+    def apply(self, game_state, calling_player, target_player):
         """
         This method should be implemented in all classes, which inherit from
         this class. It should take the current game state, perform a deepcopy,
@@ -15,6 +15,21 @@ class Card(object):
         the modified game state.
 
         :param game_state: current game state, should NOT be modified directly
+        :param calling_player: the player which called this card
+        :param target_player: the targeted player (if card does not need
+                              a target; like updating the stats of the current
+                              player, then assume that target_player = None)
         :return: modified copy of input game state
         """
         pass
+
+
+# TODO: think about constructor parameters (for base class, AbilityCard and MinionCard)
+
+
+class AbilityCard(Card):
+    pass
+
+
+class MinionCard(Card):
+    pass

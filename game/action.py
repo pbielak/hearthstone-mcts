e729ba836dddfc00a90dabbd5730c83ae899900c
Piotr Bielak <piotrbielak2@gmail.com>
2018-02-27 19:49:12 +0100

    Agreements

--- game/action.py
+++ game/action.py
@@ -12,6 +12,26 @@ class Action(object):
     """
     pass
 
+# Action:
+# * Draw card: (GameState, Player) -> GameState
+# * Play spell: (GameState,
+
+
+
+# GUI
+#       Player B Name (Health: xx/20)
+# Cards: [XYZ ABC QWE]
+# Minions: [ SuperMinion1 SuperMinion2 ]
+# ---------------------------------------
+# Minions: [ SuperMinion3 ]
+# Cards: [NMB HJK IOP]
+#       Player A Name (Health: yy/20)
+# ======================================
+# Which action?
+# 0. Take card
+# 1. Put minion on field (which one?)
+# 2. Call spell card (which one?)
+# 3. Attack with minion (which one? whom?)
 
 class Turn(object):
     """

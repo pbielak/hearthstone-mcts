73418bc083ba255108c765f827c2d8cf154cbfab
Piotr Bielak <piotrbielak2@gmail.com>
2018-03-18 14:00:43 +0100

    Tasks for report

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# Zdefiniowanie gracza agresywnego, defensywnego
+# Badanie wydajności (wykresy, czas -> głębokość węzła, średniego węzła itp.)
+# Przynajmniej 100 gier (połowa pierwszy gracz MC, połowę drugi gracz MC)
+# 100 x 3 agentów (drugi gracz)
